2c212fc8fa99ebc9f2c676bcd0ab52f785c4b5f5
sntiago16 <100149642+sntiago16@users.noreply.github.com>
2022-04-27 10:40:04 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.py
@@ -0,0 +1,29 @@
+while True:
+    basic.show_leds("""
+    . . . . .
+    . . # # #
+    # . # # #
+    # # # . .
+    . # # . .
+    """)
+    basic.pause(200)
+
+basic.show_leds("""
+    . . . . .
+    . . # # #
+    # . # # #
+    # # # . .
+    . # # . #
+    """)
+basic.pause(200)
+
+basic.show_leds("""
+    . . # # #
+    . . # # #
+    . . # . .
+    . . . . .
+    . . . . #
+    """)
+basic.pause(200)
+
+
---
+++ main.ts
@@ -0,0 +1,26 @@
+while (true) {
+    basic.showLeds(`
+    . . . . .
+    . . # # #
+    # . # # #
+    # # # . .
+    . # # . .
+    `)
+    basic.pause(200)
+}
+basic.showLeds(`
+    . . . . .
+    . . # # #
+    # . # # #
+    # # # . .
+    . # # . #
+    `)
+basic.pause(200)
+basic.showLeds(`
+    . . # # #
+    . . # # #
+    . . # . .
+    . . . . .
+    . . . . #
+    `)
+basic.pause(200)
